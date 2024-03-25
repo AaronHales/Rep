ef0a0efa8d6153b5bca8110993f52868e9546b6e
Jacob Erickson <jacobroberterickson@gmail.com>
2024-03-24 20:10:55 -0600

    login and signup less ugly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './styles/app.css';
 
 function App() {
   const authToken = useSelector(state => state.application.authToken)
   return (
-    <div>
-      <nav className="my-nav"><h2>Reptile Tracker</h2>{
+    <div id="page">
+      <nav><h2>Reptile Tracker</h2>{
         !authToken && (
-          <>
-            <Link to="/sign_up">Create Account </Link>
+          <div>
             <Link to="/login">Sign In</Link>
-          </>
+            <Link to="/sign_up">Create Account </Link>
+          </div>
         )
       }</nav>
       <Outlet />
