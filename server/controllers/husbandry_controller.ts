2d336932c0e8ab7d34bffc9707c88de3f07ccd92
Aaron Hales <a02353892@usu.edu>
2024-04-01 14:03:12 -0600

    fixed reptile deletion changes and added delete/update schedules, and delete/update feeding

--- server/controllers/husbandry_controller.ts
+++ server/controllers/husbandry_controller.ts
@@ -7,13 +7,13 @@ export const buildHusbandryController = (db: PrismaClient, husbandryRespository:
   const router = Router();
 
   router.post("/:reptileId", authMiddleware, async (req, res) => {
-    if (typeof(req.body.length) === "number" && typeof(req.body.weight) === "number" && typeof(req.body.temperature) === "number" && typeof(req.body.humidity) === "number") {
+    try {
         req.body.reptileId = +req.params.reptileId;
         const husbandryRecord = await husbandryRespository.createHusbandry(req.body);
         res.json({husbandryRecord});
     }
-    else {
-        res.status(400).json({error: "request format is {length: number, weight: number, temperature: number, humidity: number}"});
+    catch (error) {
+        res.status(400).json({error: "request format is {length: number, weight: number, temperature: number, humidity: number} " + error});
     }
   });
 
--- server/controllers/schedule_controller.ts
+++ server/controllers/schedule_controller.ts
@@ -6,50 +6,102 @@ import { ScheduleRepository } from "../repositories/schedule_respository";
 export const buildScheduleController = (db: PrismaClient, scheduleRepository: ScheduleRepository) => {
   const router = Router();
 
-  router.get("/:id", authMiddleware, async (req, res) => {
+  router.get("/:reptileId", authMiddleware, async (req, res) => {
     const schedule = await db.schedule.findFirstOrThrow({
         where: {
             userId: req.user?.id,
-            reptileId: +req.params?.id,
+            reptileId: +req.params?.reptileId,
         }
     });
     res.json({schedule});
   });
 
-  router.post("/:id", authMiddleware, async (req, res) => {
+  router.post("/:reptileId", authMiddleware, async (req, res) => {
     if (req.body.type === null || req.body.description === null || req.body.monday === null || req.body.tuesday === null || req.body.wednesday === null || 
         req.body.thursday === null || req.body.friday === null || req.body.saturday === null || req.body.sunday === null) {
             res.status(400).json({error: "request format is {type: string, description: string, monday: boolean, tuesday: boolean, wednesday: boolean, thursday: boolean, friday: boolean, saturday: boolean, sunday: boolean}"})
     }
-    else if (typeof(req.body.type) === "string" && typeof(req.body.description) === "string" && typeof(req.body.monday) === "boolean" && 
-        typeof(req.body.tuesday) === "boolean" && typeof(req.body.wednesday) === "boolean" && typeof(req.body.thursday) === "boolean" && 
-        typeof(req.body.friday) === "boolean" && typeof(req.body.saturday) === "boolean" && typeof(req.body.sunday) === "boolean") {
-            if (req.body.type.toLowerCase() === "clean" || req.body.type.toLowerCase() === "feed" || req.body.type.toLowerCase() === "record") {
-                req.body.userId = req.user?.id;
-                req.body.reptileId = +req.params.id;
-                const schedule = await scheduleRepository.createSchedule(req.body);  
-                res.json({schedule: schedule});
-                }
-        
-                else {
-                    res.status(400).json({error: req.body.type + " is not a valid schedule type"});
-                }
+    try {
+        if (req.body.type.toLowerCase() === "clean" || req.body.type.toLowerCase() === "feed" || req.body.type.toLowerCase() === "record") {
+            req.body.userId = req.user?.id;
+            req.body.reptileId = +req.params.reptileId;
+            const schedule = await scheduleRepository.createSchedule(req.body);  
+            res.json({schedule: schedule});
+        }
+        else {
+            res.status(400).json({error: req.body.type + " is not a valid schedule type"});
+        }
     }
-    else {
-        res.status(400).json({error: "request format is {type: string, description: string, monday: boolean, tuesday: boolean, wednesday: boolean, thursday: boolean, friday: boolean, saturday: boolean, sunday: boolean}"})
+    catch (error) {
+        res.status(500).json({error: "encountered an error when creating schedule" + error});
 
     }
   });
 
-  router.put("/:id/add_to_reptile", authMiddleware, async(req, res) => {
-    const scheduleToUpdate = await db.schedule.findUniqueOrThrow({
-        where: {
-            id: +req.params.id,
+    router.put("/:scheduleId", authMiddleware, async (req, res) => {
+        try {
+            const id = +req.params.scheduleId;
+            req.body.id = id;
+            const schedule = await db.schedule.findUniqueOrThrow({
+                where: {
+                    id: id,
+                }
+            });
+            if (req.body.type === null) {
+                req.body.type = schedule.type;
+            }
+            if (req.body.description === null) {
+                req.body.description = schedule.description;
+            }
+            if (req.body.monday === null) {
+                req.body.monday = schedule.monday;
+            }
+            if (req.body.tuesday === null) {
+                req.body.tuesday = schedule.tuesday;
+            }
+            if (req.body.wednesday === null) {
+                req.body.wednesday = schedule.wednesday;
+            }
+            if (req.body.thursday === null) {
+                req.body.thursday = schedule.thursday;
+            }
+            if (req.body.friday === null) {
+                req.body.friday = schedule.friday;
+            }
+            if (req.body.saturday === null) {
+                req.body.saturday = schedule.saturday;
+            }
+            if (req.body.sunday === null) {
+                req.body.sunday = schedule.sunday;
+            }
+            if (req.body.type.toLowerCase() === "clean" || req.body.type.toLowerCase() === "feed" || req.body.type.toLowerCase() === "record") {
+                const newSchedule = await scheduleRepository.updateSchedule(req.body);  
+                res.json({schedule: newSchedule});
+            }
+            else {
+                res.status(400).json({error: req.body.type + " is not a valid schedule type"});
+            }
+        }
+        catch (error) {
+            console.error("Error updating schedule:", error);
+            return res.status(500).json({ error: "An unexpected error occurred" + error });
         }
     });
-    scheduleToUpdate.reptileId = req.body.reptileId;
-    res.json({schedule: scheduleToUpdate});
-  });
+
+    router.delete("/:scheduleId", authMiddleware, async (req, res) => {
+        try {
+            const scheduleDelete = await db.schedule.delete({
+                where: {
+                    id: +req.params.scheduleId,
+                }
+            });
+            res.json({schedule: scheduleDelete});
+        }
+        catch (error) {
+            console.error("Error deleting schedule:", error);
+            return res.status(500).json({ error: "An unexpected error occurred" + error })
+        }
+    });
 
 
   return router;
